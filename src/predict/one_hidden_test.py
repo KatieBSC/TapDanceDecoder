9fe15111be4c9d79295640f6a81f49d1fda6b504
Katie <richardson.katieb@gmail.com>
2018-10-18 15:18:34 +0200

    new one_hidden NN, result analysis NB

--- src/predict/one_hidden_test.py
+++ src/predict/one_hidden_test.py
@@ -5,12 +5,15 @@ import pandas as pd
 #X_test = pd.read_csv('../../../../Source/Data/X_youtube.csv')
 #y_test = pd.read_csv('../../../../Source/Data/y_youtube.csv')
 
-data = pd.read_csv('../../../../Source/Data/youtube_data.csv')
-data = data.sample(frac=1)
+#data = pd.read_csv('../../../../Source/Data/youtube_data.csv')
+#data = data.sample(frac=1)
+
+X_test = pd.read_csv('../../../../Source/Data/X_test_mfccplus.csv')
+y_test = pd.read_csv('../../../../Source/Data/y_test_mfccplus.csv')
 
 
-X_test = data.iloc[:,2:]
-y_test = data['Labels']
+X_test = X_test.iloc[:, 1:21]
+y_test = y_test['Labels']
 
 dtype = torch.float
 device = torch.device('cpu')
@@ -19,11 +22,10 @@ x_test = torch.tensor(X_test.values, device=device, dtype=dtype)
 y_test = torch.tensor(y_test.values, device=device, dtype=torch.long).squeeze()
 
 # Load model
-model = torch.load('../train/trained_models/vanilla_mfccplus.pt')
+model = torch.load('../train/trained_models/one_hidden_20f_sig_adam.pt')
 
 outputs = model(x_test)
 y_pred = torch.max(outputs.data, 1)[1]
-
 
 # Calculate misclassification rate
 misclassifiction = 1.0 * (y_test != y_pred).sum().item() / y_pred.size()[0]
@@ -35,5 +37,4 @@ true = y_test.numpy()
 evaluation.get_errors(true, predicted)
 
 # Export results
-#evaluation.export(predicted, 'predictions/vanilla_test_mfccplus_youtube.csv')
-#evaluation.export(true, 'predictions/vanilla_test_mfccplus_youtube_true.csv')
+#evaluation.export(predicted, 'predictions/one_hidden_test_audio_20f_sig_adam.csv')
--- src/train/one_hidden_net.py
+++ src/train/one_hidden_net.py
@@ -11,12 +11,13 @@ device = torch.device("cpu")
 X_train = pd.read_csv('../../../../Source/Data/X_train_mfccplus.csv')
 y_train = pd.read_csv('../../../../Source/Data/y_train_mfccplus.csv')
 
-inputs = X_train.iloc[:,1:].values
+inputs = X_train.iloc[:,1:21].values
 targets = y_train['Labels'].values
 
 N = inputs.shape[0]
 D_in = inputs.shape[1]
 D_out = targets.max() + 1
+H = 8
 
 
 x = torch.tensor(inputs, device=device, dtype=dtype)
@@ -26,9 +27,12 @@ y = torch.tensor(targets, device=device, dtype=torch.long).squeeze()
 learning_rate = 0.005
 batch_size = 64
 
-# Neural Network
+# Neural Network with one hidden layer
 model = torch.nn.Sequential(
-    torch.nn.Linear(D_in, D_out)
+    nn.Linear(D_in, H),
+    nn.ReLU(),
+    nn.Linear(H, D_out),
+    nn.Sigmoid()
 )
 
 # Loss and optimizer
@@ -65,5 +69,5 @@ for t in epochs:
         print(t, loss.item())
 
 # Save and export trained model and training errors
-evaluation.export(loss_hist, 'train_errors/vanilla_mfccplus.csv')
-torch.save(model, 'trained_models/vanilla_mfccplus.pt')
+#evaluation.export(loss_hist, 'train_errors/one_hidden_20f_sig_adam.csv')
+#torch.save(model, 'trained_models/one_hidden_20f_sig_adam.pt')
