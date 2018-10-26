862dbb0181859a9e26f72042eef4b21b44203d3e
Katie <richardson.katieb@gmail.com>
2018-10-26 18:16:52 +0200

    added start to finish predict in pycharm

--- lib/features/__init__.py
+++ lib/features/__init__.py
@@ -6,77 +6,64 @@ from pathlib import Path
 
 # Feature generating functions
 
-class Features :
-    def __init__(self, samples, sample_rate, clip_length, n_mfcc, frame_length, hop_length, feature_set):
+class Features:
+    def __init__(self, samples, sample_rate, clip_length, n_mfcc, frame_length, hop_length):
         self.samples = samples
         self.sample_rate = sample_rate
         self.clip_length = clip_length
         self.n_mfcc = n_mfcc
         self.frame_length = frame_length
         self.hop_length = hop_length
-        self.feature_set = feature_set
-
 
     def get_features_mfcc(self):
         mfccs = np.mean(librosa.feature.mfcc(y=self.samples,
                                              sr=self.sample_rate,
                                              n_mfcc=self.n_mfcc).T, axis=0)
-        return mfccs[:,np.newaxis]
-
+        return mfccs
 
     def get_features_zcr(self):
         zcr = librosa.feature.zero_crossing_rate(self.samples,
                                                  frame_length=self.frame_length,
                                                  hop_length=self.hop_length)
-        return zcr.T
-
+        return zcr.T.flatten()
 
     def get_features_energy(self):
         energy = np.array([sum(abs(self.samples[i:i + self.frame_length] ** 2))
                            for i in range(0, len(self.samples), self.hop_length)])
-        return energy[:,np.newaxis]
-
+        return energy
 
     def get_features_rmse(self):
         rmse = librosa.feature.rmse(self.samples,
                                     frame_length=self.frame_length,
                                     hop_length=self.hop_length,
                                     center=True)
-        return rmse[0][:,np.newaxis]
-
+        return rmse[0]
 
     def get_features_bpm(self):
         onset_env = librosa.onset.onset_strength(self.samples,
                                                  sr=self.sample_rate)  # Assumes static tempo, dynamic:aggregate=None
         tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=self.sample_rate)
-        return tempo[:,np.newaxis]
+        return tempo
 
-    def get_feature_array(self):
-        feature_array = np.empty([1,1])
-        for i in self.feature_set:
-            #print(i)
+    def get_feature_array(self, feature_list):
+        feature_dict = {}
+        for i in feature_list:
             if i == 'mfcc':
-                feature_array = np.concatenate((feature_array, self.get_features_mfcc()),
-                                               axis=0)
+                feature_dict[i] = self.get_features_mfcc()
             if i == 'zcr':
-                feature_array = np.concatenate((feature_array, self.get_features_zcr()),
-                                               axis=0)
+                feature_dict[i] = self.get_features_zcr()
             if i == 'energy':
-                feature_array = np.concatenate((feature_array, self.get_features_energy()),
-                                               axis=0)
+                feature_dict[i] = self.get_features_energy()
             if i == 'rmse':
-                feature_array = np.concatenate((feature_array, self.get_features_rmse()),
-                                               axis=0)
+                feature_dict[i] = self.get_features_rmse()
             if i == 'bpm':
-                feature_array = np.concatenate((feature_array, self.get_features_bpm()),
-                                               axis=0)
-        feature_array = feature_array.flatten()
-        feature_array = np.delete(feature_array, 0)
-        return feature_array
+                feature_dict[i] = self.get_features_bpm()
+        return np.array(list(feature_dict.values()))
 
 
 # Feature utility functions
 def get_label(path):
+    path = Path(path)
     if path.parts[-3] == 'Shuffle':
         return 1
     else:
@@ -84,8 +71,8 @@ def get_label(path):
 
 
 def playback(path):
-    song = AudioSegment.from_wav(path)
-    return song
+    audio = (AudioSegment.from_wav(path))
+    return audio
 
 
 def resize_signal(path, length):
--- src/predict/__init__.py
+++ src/predict/__init__.py
@@ -1,7 +1,12 @@
-import librosa
 import features
+import torch
 
 
+# Get test data
+path = '../../../../Source/Shuffle/4/1.wav'
+
+# Listen to test data
+features.playback(path)
 
 # Parameters (set in training and validation)
 clip_length = 20772
@@ -9,16 +14,66 @@ n_mfcc = 20
 frame_length = 256
 hop_length = 128
 
-samples, sample_rate = librosa.load('../../../../Source/Shuffle/4/1.wav')
-feature_set = ['mfcc']
+# Reshape test data
+samples, sample_rate = features.resize_signal(path=path, length=clip_length)
 
-new_features = features.Features(samples=samples,
-                       sample_rate=sample_rate,
-                       clip_length=clip_length,
-                       n_mfcc=n_mfcc,
-                       frame_length=frame_length,
-                       hop_length=hop_length,
-                       feature_set=feature_set)
+# Select features
+feature_list = ['mfcc', 'bpm']
 
-print(new_features.get_feature_array().shape)
+# Get feature input data
+test_features = features.Features(samples=samples,
+                           sample_rate=sample_rate,
+                           clip_length=clip_length,
+                           n_mfcc=n_mfcc,
+                           frame_length=frame_length,
+                           hop_length=hop_length)
+
+inputs = test_features.get_feature_array(feature_list=feature_list)
+print(inputs.shape)
+print(type(inputs))
+
+# Select Model
+# model = ('..train/trained_models/one_hidden_mfcc_zcr_energy_rmse_bpm.pt')
+# model = ('..train/trained_models/one_hidden_mfcc_zcr_energy_rmse_bpm_128.pt')
+# model = ('..train/trained_models/one_hidden_mfcc_zcr_energy_rmse_bpm_256.pt')
+# model = ('..train/trained_models/two_hidden_mfcc_zcr_energy_rmse_bpm.pt')
+
+# model = ('..train/trained_models/one_hidden_mfcc_128.pt')
+# model = ('..train/trained_models/two_hidden_mfcc.pt')
+model = ('..train/trained_models/one_hidden_mfcc_bpm_128.pt')
+# model = ('..train/trained_models/two_hidden_mfcc_bpm.pt')
+
+# Load and Predict
+dtype = torch.float
+device = torch.device('cpu')
+
+inputs = torch.tensor(inputs, device=device, dtype=dtype)
+
+# Load model
+model = torch.load(model)
+
+outputs = model(inputs)
+
+y_pred = (torch.argmax(outputs.data).numpy())
+
+true = get_label(clip_path)
+
+print("What's on tap?")
+print()
+if y_pred == 1:
+    print('Predicted: Shuffle')
+elif y_pred == 0:
+    print('Predicted: Ball change')
+print()
+if true == 1:
+    print('It was a Shuffle.')
+elif true == 0:
+    print('It was a Ball change.')
+
+
+
+
+
+
+
 
